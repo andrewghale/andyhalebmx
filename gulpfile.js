e0bd952ce8358380536df151dad7a7a8216111a8
Andrew Hale <andyhalebmx@hotmail.com>
2018-11-19 15:28:36 +0000

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,40 +8,6 @@ var sass = require("gulp-sass");
 var maps = require("gulp-sourcemaps");
 var del = require("del");
 
-
-
-
-// // source and distribution folder
-// var source = "src/",
-//   dest = "dist/";
-
-// // Bootstrap scss source
-// var bootstrapSass = {
-//   in: "./node_modules/bootstrap-sass/"
-// };
-
-// // Bootstrap fonts source
-// var fonts = {
-//   in: [source + "fonts/*.*", bootstrapSass.in + "assets/fonts/**/*"],
-//   out: dest + "fonts/"
-// };
-
-// // Our scss source folder: .scss files
-// var scss = {
-//   in: source + "scss/main.scss",
-//   out: dest + "css/",
-//   watch: source + "scss/**/*",
-//   sassOpts: {
-//     outputStyle: "nested",
-//     precison: 3,
-//     errLogToConsole: true,
-//     includePaths: [bootstrapSass.in + "assets/stylesheets"]
-//   }
-// };
-
-
-
-
 gulp.task("concatScripts", function() {
   return gulp
     .src(["js/slick.js", "js/scripts.js"])
@@ -93,26 +59,6 @@ gulp.task("build", ["minifyScripts", "compileSass"], function() {
 });
 
 gulp.task("serve", ["watchFiles"]);
-
-
-
-
-
-// // copy bootstrap required fonts to dest
-// gulp.task("fonts", function() {
-//   return gulp.src(fonts.in).pipe(gulp.dest(fonts.out));
-// });
-
-// // compile scss
-// gulp.task("sass", ["fonts"], function() {
-//   return gulp
-//     .src(scss.in)
-//     .pipe(sass(scss.sassOpts))
-//     .pipe(gulp.dest(scss.out));
-// });
-
-
-
 
 // default task
 gulp.task("default", ["sass"], function() {
